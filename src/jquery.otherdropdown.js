6afff02e5b9820057b5bd49234b85b4f0dd0b6e5
Jonathan Stassen <Jonathan.Stassen@sproutsocial.com>
2017-11-22 23:05:08 -0600

    Simplify

--- src/jquery.otherdropdown.js
+++ src/jquery.otherdropdown.js
@@ -22,28 +22,19 @@
 	$.fn.otherDropdown = function(options) {
 		var $this = this;
 		// Allow a different name/value to trigger, default to 'other'
-		var opts = $.extend({}, {value: 'other'}, options);
+		var opts = $.extend({}, {value: 'other', placeholder: options.value || 'Other'}, options);
 		opts.name_lower = opts.value.toLowerCase();
-		opts.name_upper = opts.value.charAt(0).toUpperCase() + opts.value.slice(1);
-		opts.placeholder = opts.placeholder || opts.name_upper;
 
-		// Bind to all change events
+		// Prepare text input
+		var $textInput = $('<input type="text" class="otherdropdown '+opts.classes+'" placeholder="' + opts.placeholder + '" />');
+
+		// Bind to all change events to swap in the text area if 'other' option was chosen
 		$this.change( function(ev){
-
-			// Swap in the text area if our 'other' option was chosen
-			if (this.value === opts.name_lower || this.value === opts.name_upper) {
+			if (this.value.toLowerCase() === opts.name_lower) {
 				$this.hide().after( $textInput );
 				$textInput.focus();
 			}
 		});
-
-		// Prepare our text input
-		var $textInput = $('<input type="text" class="otherdropdown" placeholder="' + opts.placeholder + '" />');
-
-		// Allow custom classes on the text input
-		if (opts.classes) {
-			$textInput.addClass(opts.classes);
-		}
 
 		// Bind to blur to swap back to select dropdown
 		$textInput.blur( function(ev) {
@@ -52,11 +43,11 @@
 			this.remove();
 			$this.show();
 
-			if (value === '' || value === opts.name_lower || value === opts.name_upper) {
+			if (value === '' || this.value.toLowerCase() === opts.name_lower) {
 				return;
 			}
 
-			// If something was typed, create a new option with that value
+			// Create a new option with that value
 			var $searchedOption = $this.children('[value="' + value + '"]');
 
 			// If value doesn't exist, added it.
